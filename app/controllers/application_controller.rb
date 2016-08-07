5dc21f9150e67b560ea5c8635e65d8231d432c1b
vchaillo <valentinchaillou89@gmail.com>
2016-08-07 20:29:24 +0200

    devise user and scaffold article, like, comment

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+
+
+before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:firstname, :lastname)
+    end
+  end
+
+end
---
+++ db/migrate/20160807164733_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title,		null: false
+      t.text :text,			null: false
+      t.string :photo
+      t.string :status,		null: false
+
+      t.timestamps null: false
+    end
+  end
+end
